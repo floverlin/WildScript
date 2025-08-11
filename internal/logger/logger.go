d82a32aa557ec778e676402409635bb60167c270
floverlin <noleafloverlin@gmail.com>
2025-08-11 22:12:28 +0300

    update logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -6,3 +6,8 @@ func Log(line, col int, str string, args ...any) {
 	init := fmt.Sprintf(str, args...)
 	fmt.Printf("%s at line: %d col: %d\n", init, line, col)
 }
+
+func Slog(line, col int, str string, args ...any) string {
+	init := fmt.Sprintf(str, args...)
+	return fmt.Sprintf("%s at line: %d col: %d\n", init, line, col)
+}
